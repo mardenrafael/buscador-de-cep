2569db7159e633c6674a40359e3448598e0d7b3d
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-03-30 19:18:31 -0400

    remove some functions

--- src/CEP_search.js
+++ src/CEP_search.js
@@ -18,64 +18,6 @@ class CEP_search extends React.Component {
 
     }
 
-    handleIputChange(event) {
-        this.setState({ inputValue: event.target.value })
-    }
-
-    handleClickButton() {
-        const isValid = this.isValidFormat()
-
-
-        if (isValid) {
-        let response;
-
-        cep(this.state.inputValue)
-        .then((resp) => {
-            
-            response = resp;
-
-        }).catch((error) => {
-            const errorMessage = error.errors[0].message;
-
-            response = errorMessage;
-
-        }).finally(() => {
-
-            this.setState({ 
-            cepResponse: response,
-
-            })
-        })
-        }
-    }
-
-    /**
-     * @returns {Boolean} if inputValue is only Number and Input lenght is equal 8, return true and set elementsVisiblity hide,
-     * otherwise return false and set elementVisibilty show
-     */
-    isValidFormat() {
-        
-        const isNumber = this.isNumberOnly(this.state.inputValue);
-        const inputLength = this.state.inputValue.length;
-
-        if (isNumber && inputLength == 8) {
-        this.setState({ elementsVisiblity: "hide" });
-        return true
-        
-        } else {
-        this.setState({ elementsVisiblity: "show" })
-        return false
-        }
-    }
-
-    /**
-     * 
-     * @param {String} obj
-     * @returns {Boolean} Return boolean value if the Obj parameter is only number
-     */
-    isNumberOnly(obj) {
-        return !isNaN(obj)
-    }
 
     render() {
         return(
